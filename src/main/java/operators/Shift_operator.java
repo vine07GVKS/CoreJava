9e03b86425b7f40a348e2492edadf2829edcb002
vine07GVKS <vinethjawa2001@gmail.com>
2022-12-15 21:23:58 +0530

    shift_operator

---
+++ src/main/java/operators/Shift_operator.java
@@ -0,0 +1,10 @@
+package operators;
+
+public class Shift_operator {
+    public static void main(String[] args) {
+        int RS =25;
+        int LS=1;
+        System.out.println(RS>>LS);   // formula for right shift (a/2^b)
+        System.out.println(RS<<LS);   // formula for left shift (a*2^b)
+    }
+}
