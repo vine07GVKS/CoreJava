aea92e4b65d61ae82f9cb7610a6865a42a80324e
vine07GVKS <vinethjawa2001@gmail.com>
2022-12-15 21:09:47 +0530

    bitwise_operator

---
+++ src/main/java/first_ass/Bitwise_operator.java
@@ -0,0 +1,11 @@
+package first_ass;
+
+public class Bitwise_operator {
+    public static void main(String[] args) {
+        int X=170;
+        int Y=150;
+        System.out.println("X value = "+X+"  Y value = "+Y+"  bitwise AND operator value = "+(X&Y));
+        System.out.println("X value = "+X+"  Y value = "+Y+"  bitwise Exclusive  OR operator value = "+(X^Y));
+        System.out.println("X value = "+X+"  Y value = "+Y+"  bitwise Inclusive OR operator value = "+(X|Y));
+    }
+}
