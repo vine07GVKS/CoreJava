fbda1fb8467ba795290d92e262f654a31ddce2c7
vine07GVKS <vinethjawa2001@gmail.com>
2022-12-15 16:51:39 +0530

    arithmetic operator

---
+++ src/main/java/operators/Arethmetic_Operator.java
@@ -0,0 +1,21 @@
+package operators;
+import java.util.Scanner;
+public class Arethmetic_Operator {
+    public static void main(String[] args) {
+
+        Scanner obj=new Scanner(System.in);//object creation
+        int  Wallet=500;
+        int I_spentAmount=0;
+        System.out.println("Now My balance is "+Wallet);
+        System.out.println("Enter the total spend amount in your wallet = ");
+        I_spentAmount=obj.nextInt();
+       int myWallet=Wallet-I_spentAmount;//SUBTRACTION
+        System.out.println("Your balance amount is "+myWallet);
+        String after_Wallet="before i spend amount in wallet "+Wallet+ " after i spend remaining amount in Wallet "+myWallet;
+        System.out.println(after_Wallet);
+
+
+
+
+    }
+}
