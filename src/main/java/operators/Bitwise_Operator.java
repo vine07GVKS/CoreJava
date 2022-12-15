2f30c471edab14c9d74db1f08a3c934d4a0d24ea
vine07GVKS <vinethjawa2001@gmail.com>
2022-12-15 21:08:08 +0530

    bitwise_operator

---
+++ src/main/java/operators/Bitwise_Operator.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class Bitwise_Operator {
+    public static void main(String[] args) {
+        int X=170;
+        int Y=150;
+        System.out.println("X value = "+X+"  Y value = "+Y+"  bitwise AND operator value = "+(X&Y));
+        System.out.println("X value = "+X+"  Y value = "+Y+"  bitwise Exclusive  OR operator value = "+(X^Y));
+        System.out.println("X value = "+X+"  Y value = "+Y+"  bitwise Inclusive OR operator value = "+(X|Y));
+    }
+}
