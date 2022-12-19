f2a7ab21702991ceca35fd852b818b4fdf08ce29
vine07GVKS <vinethjawa2001@gmail.com>
2022-12-19 19:04:03 +0530

    second_ass

---
+++ src/main/java/first_ass/sec_ass.java
@@ -0,0 +1,19 @@
+package first_ass;
+public class sec_ass
+{
+    static int shop= 20 ;
+    public static void main(String[] args)
+    {
+        float myStore=shop;
+        System.out.println("using the wedding method method  "+ myStore+ "  working on the double to float");
+
+        No_Of_fruit();
+    }
+    public static void No_Of_fruit()
+    {
+
+        double costums=10;                       // using the local variable
+        float files= (float) costums;
+        System.out.println("using the narrowing method "+ files+" working on the double to float ");
+    }
+}
